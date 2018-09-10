a5046cb39f3dac9e186168939a93622fed5f4797
honghui <252860883@qq.com>
2018-09-10 19:11:28 +0800

    新增懒加载

---
+++ pages/lazyload/lazyload.js
@@ -0,0 +1,66 @@
+// pages/lazyload/lazyload.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.getImgFile( )
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  // 获取图片资源
+  getImgFile:function(){
+    let that=this;
+    wx.request({
+      url: 'https://gank.io/api/data/福利/2/1',
+      success:function(res){
+        console.log(res.data.results)
+        that.setData({
+          imgUrls: res.data.results
+        })
+      }
+    })
+  }
+})
