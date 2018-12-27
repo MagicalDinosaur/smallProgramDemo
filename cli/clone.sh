81b5f7c35c4fdb323bf91660c7b5f789827bd94f
honghui <252860883@qq.com>
2018-12-27 18:57:03 +0800

    优化shell脚本

--- cli/clone.sh
+++ cli/clone.sh
@@ -1,20 +1,66 @@
 #!/bin/bash
 
 pathname="pages"
-nums=[1,2,3]
-num=1
+template="cli/page"
+num=("1" "2" "3")
+
+# 复制page
+function inputPageName(){
+  while [ -z $pageName ]
+  do 
+    printf "\033[32mplease input page Name : \033[0m"
+    read pageName
+  done
+  targetPath="$pathname/$pageName"
+  if [ -d $targetPath ]
+    then
+      echo "\033[31mwarn: page [$pageName] is exist!\033[0m"
+  else
+    # cp 复制模版到新建路径
+    cp -iR $template $targetPath 
+    for file in `ls ./$template`
+    do
+      suffixName=${file#*.}
+      lastName="$pageName.$suffixName"
+      # mv 文件重命名
+      mv  "$targetPath/$file" "$targetPath/$lastName"
+    done 
+  echo "page [$pageName] create complete!"
+  fi
+}
+
+# 复制文件的函数
+function copyFile(){
+  # 复制page
+  if [ $type == ${num[0]} ]
+    then 
+      inputPageName $type
+  fi
+}
 
 # -z 检查字符串长度是否为0
-while [ -z $type] || [ "$type"!="1" ]
+while [ -z $type ] || [[ $type != ${num[0]} && $type != ${num[1]} && $type != ${num[2]}  ]]
 do
-  echo "\033[31m please select your file type: \033[0m"
+  echo "\033[32mplease input your file type: \033[0m"
   echo "【1】page"
   echo "【2】compoment"
   echo "【3】package"
   read type
 done
-echo "your select is $type"
+# echo "your select is $type"
+copyFile
 
+# if [ $type != "1" ]
+#   then 
+#     echo "不是1"
+# fi
+
+# a=1
+# b=1
+# if [ $a == $b ]
+# then
+#    echo "a 等于 b"
+# fi
 
 # else
 #   echo "your select is $type"
