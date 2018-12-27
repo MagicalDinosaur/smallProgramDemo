bf872500c62bbae7ec03d14504863502ff0b281e
honghui <252860883@qq.com>
2018-12-27 12:20:48 +0800

    练习 shell

--- cli/clone.sh
+++ cli/clone.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 pathname="pages"
-
-echo "\033[31m please select your file type: \033[0m"
-echo "【1】page"
-echo "【2】compoment"
-echo "【3】package"
-echo "\033[4A 光标上移4行 \033[0m"
-read type
+nums=[1,2,3]
+num=1
 
 # -z 检查字符串长度是否为0
-while [ -z $type]
+while [ -z $type] || [ "$type"!="1" ]
 do
-  echo "please select your file type:"
+  echo "\033[31m please select your file type: \033[0m"
   echo "【1】page"
   echo "【2】compoment"
   echo "【3】package"
   read type
 done
 echo "your select is $type"
+
 
 # else
 #   echo "your select is $type"
